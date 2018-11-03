63f861a68d6403c5639cf76c0c91227b8705d148
EricLiu <ericliu7@qq.com>
2018-11-02 22:45:24 -0400

    fix the error in test cases

--- Application/src/test/java/com/teqlip/database/LoginTest.java
+++ Application/src/test/java/com/teqlip/database/LoginTest.java
@@ -9,27 +9,22 @@ class LoginTest {
   @Test
   @DisplayName("check a vaild user name")
   void testForVaildUserName() {
-    assertTrue(Login.checkUserName("Eric"));
+    assertTrue(Login.checkUserName("Good"));
   }
 
   @Test
   @DisplayName("check an invaild user name")
   void testForInvaildUserName() {
-    Login.checkUserName("Violet");
-    assertFalse(Login.checkUserName("Violet"));
+    Login.createAccount("Bad", "test", "", "", "", "teqlip", "", "", 1);
+    assertFalse(Login.checkUserName("Bad"));
   }
 
   @Test
-  @DisplayName("get the first user id")
-  void testForFirstUserID() {
-    assertEquals(1, Login.newUserID());
-  }
-
-  @Test
-  @DisplayName("get the second user id")
-  void testForSecondUserID() {
-    Login.newUserID();
-    assertEquals(2, Login.newUserID());
+  @DisplayName("get the next user id")
+  void testForGetUserID() {
+    int p = Login.newUserID();
+    Login.createAccount("admin8", "test", "", "", "", "teqlip", "", "", 1);
+    assertEquals(p + 1, Login.newUserID());
   }
 
   @Test
@@ -76,53 +71,54 @@ class LoginTest {
   @Test
   @DisplayName("log in an invaild account ok pass ok")
   void testForLogInInvalidAccountWithOKPassAndAccount() {
-    Login.createAccount("admin", "test", "", "", "", "teqlip", "", "", 0);
-    assertFalse(Login.checkLoginInfo("admin", "test"));
+    Login.createAccount("admin1", "test", "", "", "", "teqlip", "", "", 0);
+    assertFalse(Login.checkLoginInfo("admin1", "test"));
   }
   
   @Test
   @DisplayName("log in a vaild account ok and pass is not ok")
   void testForLogInValidAccountWithBadPassAndOKAccount() {
-    Login.createAccount("admin", "test", "", "", "", "teqlip", "", "", 1);
-    assertFalse(Login.checkLoginInfo("admin", "bad"));
+    Login.createAccount("admin2", "test", "", "", "", "teqlip", "", "", 1);
+    assertFalse(Login.checkLoginInfo("admin2", "bad"));
   }
   
   @Test
   @DisplayName("log in an none existing account")
   void testForLogInAccountDNE() {
-    assertFalse(Login.checkLoginInfo("admin", "test"));
+    assertFalse(Login.checkLoginInfo("admin7", "test"));
   }
   
   @Test
   @DisplayName("get a user id for a account")
   void testForGetUIDForAccount() {
-    Login.createAccount("admin", "test", "", "", "", "teqlip", "", "", 1);
-    assertEquals(1, Login.getUserID("admin"));
+    int p = Login.newUserID();
+    Login.createAccount("admin3", "test", "", "", "", "teqlip", "", "", 1);
+    assertEquals(p, Login.getUserID("admin3"));
   }
   
   @Test
   @DisplayName("get a user id for an none existing account")
   void testForGetUIDForDNEAccount() {
-    assertEquals(-1, Login.getUserID("admin"));
+    assertEquals(-1, Login.getUserID("admin8"));
   }
   
   @Test
   @DisplayName("get a user status for an active account")
   void testForGetUSForActiveAccount() {
-    Login.createAccount("admin", "test", "", "", "", "teqlip", "", "", 1);
-    assertEquals(1,Login.getUserStatus("admin"));
+    Login.createAccount("admin4", "test", "", "", "", "teqlip", "", "", 1);
+    assertEquals(1,Login.getUserStatus("admin4"));
   }
   
   @Test
   @DisplayName("get a user status for an inactive account")
   void testForGetUSForInactiveAccount() {
-    Login.createAccount("admin", "test", "", "", "", "teqlip", "", "", 0);
-    assertEquals(0,Login.getUserStatus("admin"));
+    Login.createAccount("admin5", "test", "", "", "", "teqlip", "", "", 0);
+    assertEquals(0,Login.getUserStatus("admin5"));
   }
   
   @Test
   @DisplayName("get a user status for an none existing account")
   void testForGetUSForDNEAccount() {
-    assertEquals(0,Login.getUserStatus("admin"));
+    assertEquals(0,Login.getUserStatus("admin6"));
   }
 }
