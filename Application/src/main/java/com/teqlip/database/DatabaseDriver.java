fbd06bc6acc52283adc7cad414938fc44b13b235
SnoopySnipe <andrewleung.41898@outlook.com>
2018-11-09 20:48:59 -0500

    changed databasedriver

--- Application/src/main/java/com/teqlip/database/DatabaseDriver.java
+++ Application/src/main/java/com/teqlip/database/DatabaseDriver.java
@@ -11,11 +11,11 @@ public class DatabaseDriver {
    * Connect to existing database.
    * @return the database connection.
    */
-  protected static Connection connectDataBase() {
+  public static Connection connectDataBase() {
     Connection connection = null;
     try {
       Class.forName("com.mysql.jdbc.Driver");
-      connection=DriverManager.getConnection("jdbc:mysql://mathlab.utsc.utoronto.ca/cscc43f18_leunga56?useUnicode=true&useJDBCCompliantTimezoneShift=true&serverTimezone=UTC", "leunga56", "leunga56");
+      connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/assignmentdb", "root", "");
     } catch (Exception e) {
       System.out.println("Something went wrong with connection...");
       e.printStackTrace();
