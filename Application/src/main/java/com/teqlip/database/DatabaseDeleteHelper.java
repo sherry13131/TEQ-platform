baf84986889378ad704f764175074484ab4e989e
Sherry Ma <sherry.hyma@gmail.com>
2018-11-09 21:35:04 -0500

    add test cases for DatabaseSelectHelper

--- Application/src/main/java/com/teqlip/database/DatabaseDeleteHelper.java
+++ Application/src/main/java/com/teqlip/database/DatabaseDeleteHelper.java
@@ -30,5 +30,5 @@ public class DatabaseDeleteHelper {
 		}
 		return true;
 	}
-	
+
 }
--- Application/src/test/java/com/teqlip/database/DatabaseSelectorTest.java
+++ Application/src/test/java/com/teqlip/database/DatabaseSelectorTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -21,16 +22,20 @@ import com.teqlip.exceptions.DatabaseInsertException;
 
 public class DatabaseSelectorTest {
 	
+	static int user1, user2;
+	
 	@BeforeAll
-	public void setUp() throws DatabaseInsertException, SQLException {
+	public static void setUp() throws DatabaseInsertException, SQLException {
 		// insert test data row
-		DatabaseInsertHelper.createNewUserAccount("test", "1234", "fn", "ln", "", "utsc", "1234@asdf.com", "1232341234", 1);
+		user1 = DatabaseInsertHelper.createNewUserAccount("test1", "1234", "fn", "ln", null, "utsc", "1234@asdf.com", "1232341234", 1);
+		user2 = DatabaseInsertHelper.createNewUserAccount("test2", "abcd", "fn", "ln", null, "teqlip", "1234@asdf.com", "1232341234", 0);
 	}
 	
 	@AfterAll
-	public void cleanUp() {
+	public static void cleanUp() {
 		// delete all the account created here
-//		Connection con = DatabaseDriverHelper.connectDataBase();
+		DatabaseDeleteHelper.deleteAUserHelper(user1);
+		DatabaseDeleteHelper.deleteAUserHelper(user2);
 	}
 	
     @Test
@@ -108,15 +113,94 @@ public class DatabaseSelectorTest {
     }
     
     @Test
-//    @DisplayName("test checking wrong login info")
-//    public void testCorrectLogin() throws DatabaseInsertException, SQLException {
-//    	boolean status = DatabaseSelectHelper.checkLoginInfo("test", "12345");
-//    	assertTrue(status);
-//    }
-//    
-//    @Test
-//    @DisplayName("test checking wrong login info")
-//    public void testWrongLogin() {
-//    	
-//    }
+    @DisplayName("test checking correct login info")
+    public void testCorrectLogin() throws SQLException {
+    	boolean status = DatabaseSelectHelper.checkLoginInfo("test1", "1234");
+    	assertTrue(status);
+    }
+    
+    @Test
+    @DisplayName("test checking wrong login info")
+    public void testWrongLogin() throws SQLException {
+    	boolean status = DatabaseSelectHelper.checkLoginInfo("test1", "12341234");
+    	assertFalse(status);
+    }
+    
+    @Test
+    @DisplayName("test getting the active status")
+    public void testGetStatusOfUser1() throws SQLException {
+    	int status = DatabaseSelectHelper.getUserStatus("test1");
+    	assertEquals(1, status);
+    }
+    
+    @Test
+    @DisplayName("test getting the inactive status")
+    public void testGetStatusOfUser2() throws SQLException {
+    	int status = DatabaseSelectHelper.getUserStatus("test2");
+    	assertEquals(0, status);
+    }
+    
+    @Test
+    @DisplayName("test username that is not exist")
+    public void testNewUsernameThatNotExist() throws SQLException {
+    	boolean exist = DatabaseSelectHelper.checkUsernameExist("testing123");
+    	assertFalse(exist);
+    }
+    
+    @Test
+    @DisplayName("test username that not exist")
+    public void testNewUsernameThatExist() throws SQLException {
+    	boolean exist = DatabaseSelectHelper.checkUsernameExist("test1");
+    	assertTrue(exist);
+    }
+    
+    @Test
+    @DisplayName("test getting a list of usernames")
+    public void testGetUsernames() throws SQLException {
+    	List<String> usernames = DatabaseSelectHelper.getUsernames();
+    	assertTrue(usernames.contains("test1"));
+    	assertTrue(usernames.contains("test2"));
+    }
+    
+    @Test
+    @DisplayName("test getting userID of a username")
+    public void testGetUseridOfUser() throws SQLException {
+    	int id = DatabaseSelectHelper.getUserID("test1");
+    	assertEquals(user1, id);
+    }
+    
+    @Test
+    @DisplayName("test getting userID of a non exist username")
+    public void testGetUseridOfNonExistUser() throws SQLException {
+    	int id = DatabaseSelectHelper.getUserID("hellotest");
+    	assertEquals(-1, id);
+    }
+    
+    @Test
+    @DisplayName("test password match")
+    public void testCorrectPassword() throws NoSuchAlgorithmException, SQLException {
+    	boolean match = DatabaseSelectHelper.checkLoginPassword(user2, "abcd");
+    	assertTrue(match);
+    }
+    
+    @Test
+    @DisplayName("test password not match")
+    public void testWrongPassword() throws NoSuchAlgorithmException, SQLException {
+    	boolean match = DatabaseSelectHelper.checkLoginPassword(user2, "awefawe");
+    	assertFalse(match);
+    }
+    
+    @Test
+    @DisplayName("test userID exist")
+    public void testExistUserID() {
+    	boolean exist = DatabaseSelectHelper.getUserIdExist(user2);
+    	assertTrue(exist);
+    }
+    
+    @Test
+    @DisplayName("test userID not exist")
+    public void testNotExistUserID() {
+    	boolean exist = DatabaseSelectHelper.getUserIdExist(user2 + 1);
+    	assertFalse(exist);
+    }
 }
