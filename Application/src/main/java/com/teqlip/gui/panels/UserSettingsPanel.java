11145fb05b2dac98f9bd90c6a1e4912e7df83006
Anthony <anthony.leung.canada@gmail.com>
2018-11-10 21:37:06 -0500

    Retesting email send feature

---
+++ Application/src/main/java/com/teqlip/gui/panels/UserSettingsPanel.java
@@ -0,0 +1,23 @@
+package com.teqlip.gui.panels;
+
+import java.awt.event.ActionEvent;
+
+import com.teqlip.gui.frames.AppFrame;
+
+@SuppressWarnings("serial")
+public class UserSettingsPanel extends BodyPanel {
+
+	
+	
+	public UserSettingsPanel(AppFrame main) {
+		super(main);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
