232d6d1ada3ee9a59cf02f2b63b2516537aced0a
Sherry Ma <sherry.hyma@gmail.com>
2018-11-14 16:47:02 -0500

    refactor the try/catch create account code

--- Application/src/main/java/com/teqlip/gui/panels/teq/TEQCreateAccountPanel.java
+++ Application/src/main/java/com/teqlip/gui/panels/teq/TEQCreateAccountPanel.java
@@ -1,6 +1,5 @@
 package com.teqlip.gui.panels.teq;
 
-import java.awt.HeadlessException;
 import java.awt.event.*;
 import java.sql.SQLException;
 
@@ -9,27 +8,23 @@ import javax.swing.*;
 import com.teqlip.Role.RoleEnum;
 import com.teqlip.database.DatabaseInsertHelper;
 import com.teqlip.database.DatabaseSelectHelper;
-import com.teqlip.database.Login;
 import com.teqlip.exceptions.DatabaseInsertException;
 import com.teqlip.gui.frames.AppFrame;
 import com.teqlip.gui.helper.JGuiHelper;
 import com.teqlip.gui.panels.BodyPanel;
-import com.teqlip.gui.panels.BodyPanel.MenuOptions;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @SuppressWarnings("serial")
 public class TEQCreateAccountPanel extends BodyPanel {
-	
-	private BoxLayout layout;
-	
-	private Login db = new Login();
-	
-	public class ActionConsts {
-		private final static String SUBMIT = "submit";
-		private final static String CANCEL = "cancel";
-	}
+  
+  private BoxLayout layout;
+  
+  public class ActionConsts {
+    private final static String SUBMIT = "submit";
+    private final static String CANCEL = "cancel";
+  }
 
     public static final String[] ACCOUNT_FIELDS = {
         "Username",
@@ -37,11 +32,11 @@ public class TEQCreateAccountPanel extends BodyPanel {
     };
     public JTextField[] textFields = new JTextField[TEQCreateAccountPanel.ACCOUNT_FIELDS.length];
     
-	public static final Pattern VALID_EMAIL_REGEX = 
-		    Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}", Pattern.CASE_INSENSITIVE);
+  public static final Pattern VALID_EMAIL_REGEX = 
+        Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}", Pattern.CASE_INSENSITIVE);
 
     public TEQCreateAccountPanel(AppFrame main) {
-    	super(main);
+      super(main);
         layout = new BoxLayout(this, BoxLayout.PAGE_AXIS);
         setLayout(layout);
         
@@ -53,8 +48,8 @@ public class TEQCreateAccountPanel extends BodyPanel {
     }
     
     public JComponent createFieldsPane() {
-    	JPanel p = JGuiHelper.createPanelBox(BoxLayout.PAGE_AXIS);
-    	
+      JPanel p = JGuiHelper.createPanelBox(BoxLayout.PAGE_AXIS);
+      
         for (int i = 0; i < TEQCreateAccountPanel.ACCOUNT_FIELDS.length; i++) {
             JLabel label = new JLabel(TEQCreateAccountPanel.ACCOUNT_FIELDS[i]);
             this.textFields[i] = new JTextField();
@@ -62,62 +57,90 @@ public class TEQCreateAccountPanel extends BodyPanel {
             p.add(this.textFields[i]);
         }
 
-    	return p;
+      return p;
     }
     
     public JComponent createButtonPane() {
-    	JPanel p = JGuiHelper.createPanelFlow();
-    	
-    	JButton submitBtn = JGuiHelper.createButton("Submit", this, ActionConsts.SUBMIT);
-    	JButton cancelBtn = JGuiHelper.createButton("Cancel", this, ActionConsts.CANCEL);
-    	
-    	p.add(submitBtn);
-    	p.add(cancelBtn);
-    	
-    	return p;
+      JPanel p = JGuiHelper.createPanelFlow();
+      
+      JButton submitBtn = JGuiHelper.createButton("Submit", this, ActionConsts.SUBMIT);
+      JButton cancelBtn = JGuiHelper.createButton("Cancel", this, ActionConsts.CANCEL);
+      
+      p.add(submitBtn);
+      p.add(cancelBtn);
+      
+      return p;
     }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		String cmd = e.getActionCommand();
-		
-		if (ActionConsts.SUBMIT.equals(cmd)) {
-			String usernameInput = this.textFields[0].getText();
-			String emailInput = this.textFields[1].getText();
-			
-			// replace these to value in field
-			String firstName = "fn";
-			String lastName = "ln";
-			String middleName = "midname";
-			RoleEnum role = RoleEnum.TEQLIP;
-			String phoneNumber = "1234093876";
-			int active = 1;
-			// check if username exist, if yes, error
-			try {
-				if (DatabaseSelectHelper.checkUsernameExist(usernameInput)) {
-					JOptionPane.showMessageDialog(null, "Username already exist", "Fail create account - username existed", JOptionPane.ERROR_MESSAGE);
-				} else {
-					if(!checkValidEmail(emailInput)) {
-						JOptionPane.showMessageDialog(null, "Not a valid email", "Fail create account - invalid email", JOptionPane.ERROR_MESSAGE);	
-					} else {
-						// create account - default teqlip account/role
-						DatabaseInsertHelper.createNewUserAccount(usernameInput, "password", firstName, lastName, middleName, role, emailInput, phoneNumber, active);
-						JOptionPane.showMessageDialog(null, "Account created with a default password: password", "created account", JOptionPane.INFORMATION_MESSAGE);
-					}
-				}
-			} catch (HeadlessException | SQLException | DatabaseInsertException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			this.textFields[0].setText("");
-			this.textFields[1].setText("");
-		} else if (cmd.equals(ActionConsts.CANCEL)) {
-        	super.goToMenu(MenuOptions.MAIN_MENU);
-        }
-	}
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    String cmd = e.getActionCommand();
+    
+    if (ActionConsts.SUBMIT.equals(cmd)) {
+      String usernameInput = this.textFields[0].getText();
+      String emailInput = this.textFields[1].getText();
+      
+      // replace these to value in field
+      String firstName = "fn";
+      String lastName = "ln";
+      String middleName = "midname";
+      RoleEnum role = RoleEnum.TEQLIP;
+      String phoneNumber = "1234093876";
+      int active = 1;
+      
+      boolean flag = false;
+      // check if username exist, if yes, error
+      if (!checkUserExist(usernameInput)) {
+        flag = checkValidEmailHandler(emailInput);
+      }
+      if (flag) {
+        // create account - default teqlip account/role
+        addNewUserAccount(usernameInput, "password", firstName, lastName, middleName, role, emailInput, phoneNumber, active);
+      }
+      this.textFields[0].setText("");
+      this.textFields[1].setText("");
+    } else if (cmd.equals(ActionConsts.CANCEL)) {
+        super.goToMenu(MenuOptions.MAIN_MENU);
+    }
+  }
+  
+  private boolean checkUserExist(String usernameInput) {
+    try {
+      if (DatabaseSelectHelper.checkUsernameExist(usernameInput)) {
+        JOptionPane.showMessageDialog(null, "Username already exist", "Fail create account - username existed", JOptionPane.ERROR_MESSAGE);
+        return true;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return false;
+  }
+  
+  private boolean checkValidEmailHandler(String emailInput) {
+    if (checkValidEmail(emailInput)) {
+      return true;
+    }
+    JOptionPane.showMessageDialog(null, "Not a valid email", "Fail create account - invalid email", JOptionPane.ERROR_MESSAGE);
+    return false;
+  }
+  
+  private boolean addNewUserAccount(String usernameInput, String password, String firstName, String lastName, String middleName, RoleEnum role,
+      String emailInput, String phoneNumber, int active) {
+    boolean success = false;
+    try {
+      DatabaseInsertHelper.createNewUserAccount(usernameInput, password, firstName, lastName, middleName, role, emailInput, phoneNumber, active);
+      JOptionPane.showMessageDialog(null, "Account created with a default password: password", "created account", JOptionPane.INFORMATION_MESSAGE);
+      success = true;
+    } catch (DatabaseInsertException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return success;
+  }
 
-	public static boolean checkValidEmail(String emailStr) {
-	        Matcher matcher = VALID_EMAIL_REGEX .matcher(emailStr);
-	        return matcher.find();
-	}
+  private boolean checkValidEmail(String emailStr) {
+    Matcher matcher = VALID_EMAIL_REGEX .matcher(emailStr);
+    return matcher.find();
+  }
 }
