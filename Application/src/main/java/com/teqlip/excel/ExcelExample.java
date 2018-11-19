0fd10287f70d7957913c929c66bde0e35f3e8f8d
Anthony <anthony.leung.canada@gmail.com>
2018-11-18 21:29:21 -0500

    Prelimnary testing class for reading excel files

---
+++ Application/src/main/java/com/teqlip/excel/ExcelExample.java
@@ -0,0 +1,49 @@
+package com.teqlip.excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+ 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelExample {
+	public static void main(String[] args) throws IOException {
+        String excelFilePath = "Books.xlsx";
+        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+         
+        Workbook workbook = new XSSFWorkbook(inputStream);
+        Sheet firstSheet = workbook.getSheetAt(0);
+        Iterator<Row> iterator = firstSheet.iterator();
+         
+        while (iterator.hasNext()) {
+            Row nextRow = iterator.next();
+            Iterator<Cell> cellIterator = nextRow.cellIterator();
+             
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next(); // cell.getCellTypeEnum();
+                 
+                switch (cell.getCellType()) { 
+                    case Cell.CELL_TYPE_STRING:
+                        System.out.print(cell.getStringCellValue());
+                        break;
+                    case Cell.CELL_TYPE_BOOLEAN:
+                        System.out.print(cell.getBooleanCellValue());
+                        break;
+                    case Cell.CELL_TYPE_NUMERIC:
+                        System.out.print(cell.getNumericCellValue());
+                        break;
+                }
+                System.out.print(" - ");
+            }
+            System.out.println();
+        }
+         
+        workbook.close();
+        inputStream.close();
+    }
+}
